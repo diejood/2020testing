ae552aed8a9f476fcee79368db3ed6b63ea9ea99
Hein Eksteen <hein1@hotmail.co.uk>
2020-08-17 14:48:38 +1200

    REQ2- Except user input and store.

--- src/main/java/nz/ac/eit/amazingtranslator/App.java
+++ src/main/java/nz/ac/eit/amazingtranslator/App.java
@@ -1,5 +1,7 @@
 package nz.ac.eit.amazingtranslator;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,7 +10,8 @@ public class App
 {
     public static void main( String[] args )
     {
-
         System.out.println( "Hello World!" );
+        Scanner scanner=new Scanner(System.in);
+        String numberAsString=scanner.nextLine();
     }
 }
